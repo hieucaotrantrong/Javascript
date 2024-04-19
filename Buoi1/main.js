bd332502aa065268e87bfd8a12730eedd4a4bf19
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-04-19 14:31:57 +0700

    Javascript

---
+++ Buoi1/main.js
@@ -0,0 +1,16 @@
+
+var fullName = "Cao Tran Trong Hieu";
+var age = 21;
+alert(fullName);
+alert(age);
+
+var fullName = 'Cao Tran Trong Hieu'
+console.log(fullName)
+// confirm('Xac Nhan ban du tuoi')
+
+setTimeout(function () {
+    console.log('Thong bao Hieu Da yeu Phuong')
+}, 2000);
+setInterval(function () {
+    console.log('Day la log' + Math.random)
+}, 1000)
---
+++ Buoi2/main.js
@@ -0,0 +1,71 @@
+//  Math trong Javascript
+//  Toán tử số học
+/*
+var a = 3;
+var b = 2;
+a++;
+var c = a + b;
+console.log(c)*/
+//  Toan tu tien to va hau to
+/*
+var a = 6;
+var output = a++;
+console.log('output=' + output)
+console.log('a=' + a)
+//   exemple
+var number = 6;
+var output = number++ * 2 - --number * 2;
+// 6*2-6*2
+console.log('output=' + output)*/
+//  toán tử gán
+/*
+var a = 2;
+a *= 2;
+console.log(a);
+*/
+//   Toán  tử chuỗi
+/*
+var firtName = 'Hieu';
+var lastName = 'Phuong';
+console.log(firtName + ' ' + lastName);
+*/
+//   Toán tử so sánh
+/*
+var a = 'Hieu';
+var b = 'Phuong';
+if (a >= b) {
+    console.log('Dieu kien dung')
+} else {
+    console.log('DIeu kien sai')
+}*/
+//  kiểu dữ liệu boolean
+/*
+var a = true;
+console.log(a)*/
+//  toán  tử logic
+/*
+  Toán tử và
+var a = 1;
+var b = 2;
+var c = 2;
+if (a > 0 && b > 0 && c > 0) {
+    console.log("Dieu kien dung")
+} else {
+    console.log("Dieu kien sai")
+}*/
+/*
+ Toán tử hoặc or
+var a = 1; b = 2; c = 3;
+if (a > 0 || b < 0 || a < 0) {
+    console.log("Dieu kien dung")
+} else {
+    console.log("Dieu kien sai")
+}*/
+//   Toán tử not
+/*
+var a = 3; b = 2; c = 2;
+if (!(a < 0)) {
+    console.log('Dieu kien dung')
+} else {
+    console.log('Dieu kien sai')
+}*/
